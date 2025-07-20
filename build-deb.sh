1ce7ba2e38ab0b24a7cfe96490493ef924df61cf
gfdgd_xi <3025613752@qq.com>
2025-07-20 12:10:31 +0800

    init

---
+++ build-deb.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# install dep
+echo "deb-src [trusted=true] https://mirrors.kernel.org/deepin/beige/ crimson main commercial community" | tee /etc/apt/sources.list.d/deepin-sources.list
+sudo apt update
+sudo apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev
+sudo apt install -y gcc-loongarch64-linux-gnu
+sudo apt build-dep -y linux
+
+git clone https://github.com/deepin-community/kernel --depth=1 -b linux-6.12.y
+
+cd kernel
+# 删除 .git 目录以避免版本号带 commit
+rm -rf .git
+
+make ARCH=loongarch CROSS_COMPILE=loongarch64-linux-gnu- deepin_loongarch_desktop_defconfig
+
+scripts/config --undefine CONFIG_DEBUG_INFO
+scripts/config --undefine CONFIG_DEBUG_INFO_DWARF5
+scripts/config --undefine CONFIG_DEBUG_INFO_COMPRESSED_NONE 
+scripts/config --undefine CONFIG_PAHOLE_HAS_SPLIT_BTF
+scripts/config --undefine CONFIG_PAHOLE_HAS_LANG_EXCLUDE
+scripts/config --undefine CONFIG_GDB_SCRIPTS
+
+scripts/config --set-val CONFIG_DEBUG_INFO_NONE y
+
+scripts/config --undefine CONFIG_HAVE_PAGE_SIZE_16KB
+scripts/config --undefine CONFIG_PAGE_SIZE_16KB
+scripts/config --undefine CONFIG_16KB_3LEVEL
+
+scripts/config --set-val CONFIG_PAGE_SHIFT 12
+scripts/config --set-val CONFIG_HAVE_PAGE_SIZE_4KB y
+scripts/config --set-val CONFIG_PAGE_SIZE_4KB y
+scripts/config --set-val CONFIG_4KB_3LEVEL y
+
+
+scripts/config --set-str CONFIG_LOCALVERSION "-loong64-4k-pagesize-gxde-desktop"
+
+# build deb packages
+CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
+env DEBEMAIL="gfdgd xi <3025613752@qq.com>" make DPKG_FLAGS=-d ARCH=loongarch CROSS_COMPILE=loongarch64-linux-gnu- bindeb-pkg -j"$CPU_CORES"
+
+cd ..
+rm -rf linux-libc-dev*.deb *dbg*.deb
+mv *.deb ..
