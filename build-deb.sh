c0c2de9c28240dbadd84393413b90398519736d7
gfdgd_xi <3025613752@qq.com>
2025-07-20 16:24:24 +0800

    调整依赖安装顺序

--- build-deb.sh
+++ build-deb.sh
@@ -3,13 +3,20 @@
 echo "deb-src [trusted=true] https://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware" | tee /etc/apt/sources.list.d/deepin-sources.list
 apt update
 apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git vim libelf-dev sudo
+apt build-dep -y linux
 apt install -y gcc-loongarch64-linux-gnu g++-loongarch64-linux-gnu binutils-loongarch64-linux-gnu \
     cpp-loongarch64-linux-gnu
-apt build-dep -y linux
 
 git clone https://github.com/deepin-community/kernel --depth=1 -b linux-6.12.y
 
 cd kernel
+
+# 检测 build-version 脚本是否存在
+if [[ ! -f init/build-version ]]; then
+    cp ../build-version init -rv
+    chmod +x init/build-version
+fi
+
 # 删除 .git 目录以避免版本号带 commit
 rm -rf .git
 
@@ -17,14 +24,14 @@ rm -rf .git
 
 make ARCH=loongarch CROSS_COMPILE=loongarch64-linux-gnu- deepin_loongarch_desktop_defconfig
 
-scripts/config --undefine CONFIG_DEBUG_INFO
-scripts/config --undefine CONFIG_DEBUG_INFO_DWARF5
-scripts/config --undefine CONFIG_DEBUG_INFO_COMPRESSED_NONE 
-scripts/config --undefine CONFIG_PAHOLE_HAS_SPLIT_BTF
-scripts/config --undefine CONFIG_PAHOLE_HAS_LANG_EXCLUDE
-scripts/config --undefine CONFIG_GDB_SCRIPTS
+#scripts/config --undefine CONFIG_DEBUG_INFO
+#scripts/config --undefine CONFIG_DEBUG_INFO_DWARF5
+#scripts/config --undefine CONFIG_DEBUG_INFO_COMPRESSED_NONE 
+#scripts/config --undefine CONFIG_PAHOLE_HAS_SPLIT_BTF
+#scripts/config --undefine CONFIG_PAHOLE_HAS_LANG_EXCLUDE
+#scripts/config --undefine CONFIG_GDB_SCRIPTS
 
-scripts/config --set-val CONFIG_DEBUG_INFO_NONE y
+#scripts/config --set-val CONFIG_DEBUG_INFO_NONE y
 
 scripts/config --undefine CONFIG_HAVE_PAGE_SIZE_16KB
 scripts/config --undefine CONFIG_PAGE_SIZE_16KB
