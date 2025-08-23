4b893bd492d645e77e89178c41b4ddc0b5d4f227
gfdgd_xi <3025613752@qq.com>
2025-08-23 18:21:48 +0800

    调整自动构建
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

--- build-deb.sh
+++ build-deb.sh
@@ -9,6 +9,12 @@ apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio lib
 apt build-dep -y linux
 apt install -y gcc-loongarch64-linux-gnu g++-loongarch64-linux-gnu binutils-loongarch64-linux-gnu \
     cpp-loongarch64-linux-gnu
+apt install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu binutils-aarch64-linux-gnu \
+    cpp-aarch64-linux-gnu
+apt install -y gcc-mips64el-linux-gnuabi64 g++-mips64el-linux-gnuabi64 binutils-mips64el-linux-gnuabi64 \
+    cpp-mips64el-linux-gnuabi64
+apt install -y gcc-riscv64-linux-gnu g++-riscv64-linux-gnu binutils-riscv64-linux-gnu \
+    cpp-riscv64-linux-gnu
 
 git clone https://github.com/deepin-community/kernel --depth=1 -b linux-6.6.y
 
@@ -23,10 +29,27 @@ fi
 # 删除 .git 目录以避免版本号带 commit
 rm -rf .git
 
-# 用于解决交叉编译缺失 libssl-dev 头文件的问题
-#export C_INCLUDE_PATH=/usr/include/x86_64-linux-gnu:$C_INCLUDE_PATH
+if [[ $GXDE_CROSS_ARCH == "amd64" ]]; then
+    make ARCH=x86 deepin_x86_desktop_defconfig
+fi
+if [[ $GXDE_CROSS_ARCH == "arm64" ]]; then
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- deepin_arm64_desktop_defconfig
+fi
+if [[ $GXDE_CROSS_ARCH == "mips64el" ]]; then
+    make ARCH=mips CROSS_COMPILE=mips64el-linux-gnuabi64- deepin_loongson3_desktop_defconfig
+fi
+if [[ $GXDE_CROSS_ARCH == "loong64" ]]; then
+    make ARCH=loongarch CROSS_COMPILE=loongarch64-linux-gnu- deepin_loongarch_desktop_defconfig
+fi
+if [[ $GXDE_CROSS_ARCH == "riscv64" ]]; then
+    make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- deepin_riscv64_desktop_defconfig
+fi
 
-make ARCH=loongarch CROSS_COMPILE=loongarch64-linux-gnu- deepin_loongarch_desktop_defconfig
+if [[ $GXDE_CROSS_ARCH == "loong64" ]]; then
+    scripts/config --set-str CONFIG_LOCALVERSION "-deepin-loong64-4k-pagesize-gxde-desktop"
+else
+    scripts/config --set-str CONFIG_LOCALVERSION "-deepin-$GXDE_CROSS_ARCH-gxde-desktop"
+fi
 
 scripts/config --undefine CONFIG_DEBUG_INFO
 scripts/config --undefine CONFIG_DEBUG_INFO_DWARF5
@@ -37,21 +60,22 @@ scripts/config --undefine CONFIG_GDB_SCRIPTS
 
 scripts/config --set-val CONFIG_DEBUG_INFO_NONE y
 
-scripts/config --undefine CONFIG_HAVE_PAGE_SIZE_16KB
-scripts/config --undefine CONFIG_PAGE_SIZE_16KB
-scripts/config --undefine CONFIG_16KB_3LEVEL
+# Loongarch 内核使用 4k 分页
+if [[ $GXDE_CROSS_ARCH == "loong64" ]]; then
+    scripts/config --undefine CONFIG_HAVE_PAGE_SIZE_16KB
+    scripts/config --undefine CONFIG_PAGE_SIZE_16KB
+    scripts/config --undefine CONFIG_16KB_3LEVEL
+
+    scripts/config --set-val CONFIG_PAGE_SHIFT 12
+    scripts/config --set-val CONFIG_HAVE_PAGE_SIZE_4KB y
+    scripts/config --set-val CONFIG_PAGE_SIZE_4KB y
+    scripts/config --set-val CONFIG_4KB_3LEVEL y
+fi
 scripts/config --undefine CONFIG_SYSTEM_TRUSTED_KEYRING
 scripts/config --undefine CONFIG_SYSTEM_TRUSTED_KEYS
 scripts/config --undefine CONFIG_MODULE_SIG_KEY
 scripts/config --undefine CONFIG_MODULE_SIG_KEY_TYPE_RSA
 
-scripts/config --set-val CONFIG_PAGE_SHIFT 12
-scripts/config --set-val CONFIG_HAVE_PAGE_SIZE_4KB y
-scripts/config --set-val CONFIG_PAGE_SIZE_4KB y
-scripts/config --set-val CONFIG_4KB_3LEVEL y
-
-
-scripts/config --set-str CONFIG_LOCALVERSION "-loong64-4k-pagesize-gxde-desktop"
 
 # build deb packages
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
