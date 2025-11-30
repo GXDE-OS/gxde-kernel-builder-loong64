08f28be10f10e3b3811ae576d7f0ff56c99c5486
gfdgd_xi <3025613752@qq.com>
2025-11-30 15:57:54 +0800

    调整loongarch,mips构建方式
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

--- build-deb.sh
+++ build-deb.sh
@@ -15,10 +15,12 @@ apt install -y gcc-mips64el-linux-gnuabi64 g++-mips64el-linux-gnuabi64 binutils-
     cpp-mips64el-linux-gnuabi64
 apt install -y gcc-riscv64-linux-gnu g++-riscv64-linux-gnu binutils-riscv64-linux-gnu \
     cpp-riscv64-linux-gnu
+apt install -y gcc-i686-linux-gnu g++-i686-linux-gnu binutils-i686-linux-gnu \
+    cpp-i686-linux-gnu
 
-git clone https://github.com/GXDE-OS/kernel-6.6.y --depth=1 -b linux-6.6.y
+git clone https://github.com/GXDE-OS/kernel --depth=1 -b linux-6.6.y
 
-cd kernel-6.6.y
+cd kernel
 
 # 检测 build-version 脚本是否存在
 if [[ ! -f init/build-version ]]; then
@@ -31,6 +33,9 @@ rm -rf .git
 
 export DEBEMAIL="gfdgd xi <3025613752@qq.com>"
 
+if [[ $GXDE_CROSS_ARCH == "i386" ]]; then
+    make ARCH=arm64 CROSS_COMPILE=i686-linux-gnu- deepin_x86_desktop_defconfig
+fi
 if [[ $GXDE_CROSS_ARCH == "amd64" ]]; then
     make ARCH=x86 deepin_x86_desktop_defconfig
 fi
@@ -47,15 +52,6 @@ if [[ $GXDE_CROSS_ARCH == "riscv64" ]]; then
     make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- deepin_riscv64_desktop_defconfig
 fi
 
-if [[ $GXDE_CROSS_ARCH == "loong64" ]]; then
-    scripts/config --set-str CONFIG_LOCALVERSION "-deepin-loong64-4k-pagesize-gxde-desktop"
-else
-    scripts/config --set-str CONFIG_LOCALVERSION "-deepin-$GXDE_CROSS_ARCH-gxde-desktop"
-fi
-if [[ $GXDE_CROSS_ARCH == "mips64el" ]]; then
-    scripts/config --set-str CONFIG_LOCALVERSION "-deepin-mips64el-4k-pagesize-gxde-desktop"
-fi
-
 scripts/config --undefine CONFIG_DEBUG_INFO
 scripts/config --undefine CONFIG_DEBUG_INFO_DWARF5
 scripts/config --undefine CONFIG_DEBUG_INFO_COMPRESSED_NONE 
@@ -64,18 +60,6 @@ scripts/config --undefine CONFIG_PAHOLE_HAS_LANG_EXCLUDE
 scripts/config --undefine CONFIG_GDB_SCRIPTS
 
 scripts/config --set-val CONFIG_DEBUG_INFO_NONE y
-
-# Loongarch 内核使用 4k 分页
-if [[ $GXDE_CROSS_ARCH == "loong64" ]]; then
-    scripts/config --undefine CONFIG_HAVE_PAGE_SIZE_16KB
-    scripts/config --undefine CONFIG_PAGE_SIZE_16KB
-    scripts/config --undefine CONFIG_16KB_3LEVEL
-
-    scripts/config --set-val CONFIG_PAGE_SHIFT 12
-    scripts/config --set-val CONFIG_HAVE_PAGE_SIZE_4KB y
-    scripts/config --set-val CONFIG_PAGE_SIZE_4KB y
-    scripts/config --set-val CONFIG_4KB_3LEVEL y
-fi
 scripts/config --undefine CONFIG_SYSTEM_TRUSTED_KEYRING
 scripts/config --undefine CONFIG_SYSTEM_TRUSTED_KEYS
 scripts/config --undefine CONFIG_MODULE_SIG_KEY
@@ -85,6 +69,9 @@ scripts/config --undefine CONFIG_MODULE_SIG_KEY_TYPE_RSA
 # build deb packages
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
 
+if [[ $GXDE_CROSS_ARCH == "i386" ]]; then
+    make ARCH=arm64 CROSS_COMPILE=i686-linux-gnu- DPKG_FLAGS=-d bindeb-pkg -j"$CPU_CORES"
+fi
 if [[ $GXDE_CROSS_ARCH == "amd64" ]]; then
     make ARCH=x86 DPKG_FLAGS=-d bindeb-pkg -j"$CPU_CORES"
 fi
@@ -101,6 +88,25 @@ if [[ $GXDE_CROSS_ARCH == "riscv64" ]]; then
     make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- DPKG_FLAGS=-d bindeb-pkg -j"$CPU_CORES"
 fi
 
+# 再重新编译个 4k pagesize 内核（mips, loongarch）
+if [[ $GXDE_CROSS_ARCH == "loong64" ]] || [[ $GXDE_CROSS_ARCH == "mips64el" ]]; then
+    scripts/config --set-str CONFIG_LOCALVERSION "-$GXDE_CROSS_ARCH-4k-pagesize-gxde-desktop"
+    if [[ $GXDE_CROSS_ARCH == "loong64" ]]; then
+        scripts/config --undefine CONFIG_HAVE_PAGE_SIZE_16KB
+        scripts/config --undefine CONFIG_PAGE_SIZE_16KB
+        scripts/config --undefine CONFIG_16KB_3LEVEL
+        scripts/config --set-val CONFIG_PAGE_SHIFT 12
+        scripts/config --set-val CONFIG_HAVE_PAGE_SIZE_4KB y
+        scripts/config --set-val CONFIG_PAGE_SIZE_4KB y
+        scripts/config --set-val CONFIG_4KB_3LEVEL y
+        make ARCH=loongarch CROSS_COMPILE=loongarch64-linux-gnu- DPKG_FLAGS=-d bindeb-pkg -j"$CPU_CORES"
+    fi
+    if [[ $GXDE_CROSS_ARCH == "mips64el" ]]; then
+        scripts/config --set-val CONFIG_PAGE_SIZE_4KB y
+        scripts/config --undefine CONFIG_PAGE_SIZE_16KB
+        make ARCH=mips CROSS_COMPILE=mips64el-linux-gnuabi64- DPKG_FLAGS=-d bindeb-pkg -j"$CPU_CORES"
+    fi
+fi
 
 cd ..
 rm -rf linux-libc-dev*.deb *dbg*.deb
