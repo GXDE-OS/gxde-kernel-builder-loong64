c4d1f74c2f8db90d289a0d9e5fe484d87aa16135
gfdgd_xi <3025613752@qq.com>
2026-05-07 21:54:28 +0800

    修复riscv64安装问题
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

--- build-deb.sh
+++ build-deb.sh
@@ -21,7 +21,8 @@ apt install -y gcc-riscv64-linux-gnu g++-riscv64-linux-gnu binutils-riscv64-linu
 apt install -y gcc-i686-linux-gnu g++-i686-linux-gnu binutils-i686-linux-gnu \
     cpp-i686-linux-gnu
 # 安装对应架构的 libssl-dev 包，否则编译内核时会提示找不到 openssl/sha.h 头文件
-apt install -y libssl-dev:$GXDE_CROSS_ARCH
+apt install -y libssl-dev:$GXDE_CROSS_ARCH libc6:$GXDE_CROSS_ARCH libelf-dev:$GXDE_CROSS_ARCH libdw-dev:$GXDE_CROSS_ARCH
+apt install -y python3:$GXDE_CROSS_ARCH
 
 git clone https://github.com/GXDE-OS/kernel --depth=1 -b $VERSION
 
